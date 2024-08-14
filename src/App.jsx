8eaa776b1d1e7e79bb0ed3faca2c9e27acc88fd7
Imran Muhammad <imranwebdevelop@gmail.com>
2024-08-14 13:39:48 +0300

    Start the project and clear the test code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,5 @@
-const data = ['Ahmed', 'Isa', 'Imran', 'Amir', 'Ismael'];
-
 function App() {
-  const time = new Date().toLocaleTimeString();
-
-  return (
-    <>
-      <h1>Hello React With vite.{time}</h1>
-      <List />
-      <Avater />
-    </>
-  );
-}
-
-function List() {
-  return (
-    <ul>
-      {data.map(name => (
-        <li key={name}>{name}</li>
-      ))}
-    </ul>
-  );
-}
-
-function Avater() {
-  return <img src="./src/assets/images/LinkedIn_Learning.jpg" alt="Img" />;
+  return <h1>Test</h1>;
 }
 
 export default App;
